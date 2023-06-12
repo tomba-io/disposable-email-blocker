65c688412ca26146fd68a4e00ba487eb98c8bb65
Mohamed Ben <b.mohamed@tomba.io>
2023-06-12 02:00:36 +0100

    chore: add options

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -3,3 +3,27 @@ export interface TombaStatusResponse {
     webmail: boolean;
     disposable: boolean;
 }
+
+export interface Options {
+    disposable?: DisposableOptions;
+    webmail?: WebmailOptions;
+    emailError?: ErrorOptions;
+}
+export interface DefaultOptions {
+    disposable: DisposableOptions;
+    webmail: WebmailOptions;
+    emailError: ErrorOptions;
+}
+
+export interface WebmailOptions {
+    message: string;
+    block?: boolean;
+}
+export interface DisposableOptions {
+    message: string;
+}
+
+export interface ErrorOptions {
+    className?: string;
+    style?: string;
+}
